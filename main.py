e3180ab8cde38cdc9a4b2d7b8f31b348a1cd609a
CyberFlame <cyberflameu@gmail.com>
2022-03-24 10:10:25 +1300

    gtg

--- main.py
+++ main.py
@@ -26,12 +26,12 @@ class Player(object):
     """
     Player class oh yeah
     """
-
+    # TODO: add display width/height as params here
     def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.width: int = width
+        self.height: int = height
+        self.x = (DISPLAY_WIDTH - self.width) // 2
+        self.y = DISPLAY_HEIGHT - self.height
         self.velocity = 5
         self.bounding_box = None  # TODO: use this variable to detect if on platform
 
@@ -42,6 +42,13 @@ def main():
     """
 
     player = Player(DISPLAY_WIDTH, DISPLAY_HEIGHT - 65, 40, 60)
+    width: int = 40
+    height: int = 60
+    x = (DISPLAY_WIDTH - width) // 2
+    y = DISPLAY_HEIGHT - height
+    velocity = 5
+    width: int = 40
+    height: int = 60
     pygame.display.set_caption("Jumpy Blob")
     playing: bool = True
     while playing:
@@ -51,10 +58,23 @@ def main():
                 playing = False
             if event.type == pygame.KEYDOWN:
                 pass
+
         keys: Sequence[bool] = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
+            if x > velocity:
+                x -= velocity
+        if keys[pygame.K_RIGHT]:
+            if x < DISPLAY_WIDTH - width - velocity:
+                x += velocity
+        display_surface.fill(BLACK)
+        red_rectangle = (
+            pygame.draw.rect(display_surface, DEEP_RED, (x, y, width, height))
+        )
+        if (
+                red_rectangle  # remove this later when there's a proper use for the variable.
+        ):
             pass
-
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
