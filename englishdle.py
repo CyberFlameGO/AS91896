f0c85c202c0424c9f30c568049b60f262536191b
CyberFlame <cyberflameu@gmail.com>
2022-04-01 15:16:18 +1300

    dsf

--- englishdle.py
+++ englishdle.py
@@ -1,6 +1,21 @@
 """
 Python Wordle thing
 """
-import utils
 
-print(utils.Utils(r'./wordlist.txt').load_words())
+import random
+import json
+
+
+def get_word_of_length(words, length):
+    # (filter is just a wrapper for a generator by the looks of it)
+    filter(lambda x: len(x)==length, words)  # https://stackoverflow.com/questions/26697601/how-to-return-all-list-elements-of-a-given-length
+
+def main():
+    """
+    Main
+    """
+    raw_words = r'./wordlist.json'
+    with open(raw_words) as word_list:
+        wordlist = list(json.load(word_list).keys())
+    word = random.choice(wordlist)
+
